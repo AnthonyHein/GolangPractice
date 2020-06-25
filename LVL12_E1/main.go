f0ac8ed803ba6b1aacd448cf286c7e760b5acd29
Anthony Hein <anhein@princeton.edu>
2020-06-24 20:36:21 -0400

    Testing godoc.

---
+++ LVL12_E1/main.go
@@ -0,0 +1,12 @@
+// Implements an easy way to go between human years and dog years.
+package main
+
+import (
+    "fmt"
+    "UdemyCourse/LVL12_E1/dog"
+)
+
+// Tests the Years function.
+func main () {
+    fmt.Println(dog.Years(7))
+}
